bdc2117966de383234be1b3d451f4b5f01ae9a7e
Harshit Maheshwari <harshitmah98@gmail.com>
2020-01-02 20:04:11 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/spotifyalarmclock/APIKeys.java
@@ -0,0 +1,5 @@
+package com.example.spotifyalarmclock;
+
+public class APIKeys {
+    public final static String SPOTIFY_CLIENT_ID = "1268c64f487441f5b757c43092759207";
+}
---
+++ app/src/main/java/com/example/spotifyalarmclock/MainActivity.java
@@ -0,0 +1,27 @@
+ package com.example.spotifyalarmclock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
